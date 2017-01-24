de6d8d9bdbf710d25d88f2a78b597c08c0d75429
Chris Metcalf <chris@chrismetcalf.net>
2017-01-23 18:06:06 -0800

    Email is more unique than display name
    But we need to replace out the "@"

--- oauthenticator/socrata.py
+++ oauthenticator/socrata.py
@@ -91,4 +91,4 @@ class SocrataOAuthenticator(OAuthenticator):
         resp = yield http_client.fetch(req)
         resp_json = json.loads(resp.body.decode('utf8', 'replace'))
 
-        return resp_json["displayName"]
+        return resp_json["email"].replace("@", "_at_")
